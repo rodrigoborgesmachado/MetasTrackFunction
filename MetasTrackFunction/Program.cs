e45201c232bb45801b223c8332dca75aa9da5b9c
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2024-01-04 17:49:03 -0300

    Alterando para copiar arquivo.

--- MetasTrackFunction/Program.cs
+++ MetasTrackFunction/Program.cs
@@ -8,7 +8,7 @@ public static class ScheduledFunction
 {
     [FunctionName("ScheduledFunction")]
     public static void Run(
-        [TimerTrigger("0 40 20 * * *")] TimerInfo myTimer,
+        [TimerTrigger("0 55 20 * * *")] TimerInfo myTimer,
         ILogger log)
     {
         Console.WriteLine("Calling process");
